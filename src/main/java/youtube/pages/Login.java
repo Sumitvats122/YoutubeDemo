737ad11f25228ebc947ecc722eab5320c29ce9c3
unknown <Sumitvats122>
2020-10-08 14:57:47 +0530

    updated Login

--- src/main/java/youtube/pages/Login.java
+++ src/main/java/youtube/pages/Login.java
@@ -58,21 +58,10 @@ public class Login extends Base {
 //			driver.findElement(By.id("subscribe-button")).click();
 //			Thread.sleep(2000);
 			
-			//for logout
-//			driver.findElement(By.id("img")).click();
-//			Thread.sleep(5000);
-//			driver.findElement(By.xpath("//yt-formatted-string[@id='label'][contains(text(),'Sign out')]")).click();
-//			Thread.sleep(5000);
+
 
 					
-//			List<WebElement> list = driver.findElements(By.xpath("//div[@class='sbdd_b']//ul//li"));
-//			System.out.println(list.size());
-//			for(int i = 0; i < list.size(); i++)
-//			{
-//				System.out.println(i);
-//			}
-			
-			//driver.close();
+
 	}
 
 
